e4c15d826be11475e24981b1b634597330b6f172
Nathan Woodward <nathanrichardwoodward@gmail.com>
2017-04-29 09:52:02 +0100

    Implemented OpenGL animation rendering

--- src/pygame_opengl_renderer.py
+++ src/pygame_opengl_renderer.py
@@ -8,20 +8,74 @@ from pymunk import Vec2d
 from .renderer import *
 
 class Texture(object):
+    """ An OpenGL texture. """
+
     def __init__(self, filename):
-        self.__surface = pygame.image.load(filename)
+        """ Constructor. """
+        surface = pygame.image.load(filename).convert_alpha()
+        data = pygame.image.tostring(surface, "RGBA", 1)
+        self.__width = surface.get_width()
+        self.__height = surface.get_height()
+        self.__texture = GL.glGenTextures(1)
+        GL.glEnable(GL.GL_TEXTURE_2D)
+        GL.glBindTexture(GL.GL_TEXTURE_2D, self.__texture)
+        GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_LINEAR)
+        GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR)
+        GL.glTexImage2D(GL.GL_TEXTURE_2D, 0, GL.GL_RGBA, self.get_width(), self.get_height(),
+                        0, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, data)
+        GL.glDisable(GL.GL_TEXTURE_2D)
+
+    def begin(self):
+        """ Set OpenGL state. """
+        GL.glEnable(GL.GL_TEXTURE_2D)
+        GL.glBindTexture(GL.GL_TEXTURE_2D, self.__texture)
+        GL.glColor3f(1, 1, 1)
+
+    def end(self):
+        """ Unset the state. """
+        GL.glDisable(GL.GL_TEXTURE_2D)
+
     def get_width(self):
-        return self.__surface.get_width()
+        """ Get the texture width in pixels. """
+        return self.__width
+
     def get_height(self):
-        return self.__surface.get_height()
+        """ Get the texture height in pixels. """
+        return self.__height
 
 class TextureSequence(object):
+    """ A sequence of textures. """
+
+    # Note: this is not really a practical implementation, we have lots of
+    # large frames per anim so that's lots of textures. It would be good to
+    # use a texture atlas or array texture for the frames rather than a
+    # texture per frame.
+
     def __init__(self, filenames):
-        self.__surface = pygame.image.load(filenames[0])
+        """ Constructor. """
+        self.__textures = [Texture(f) for f in filenames]
+        self.__bound_texture = None
+
+    def begin(self, timer):
+        """ Set the state. """
+        assert self.__bound_texture is None
+        idx = timer.pick_index(len(self.__textures))
+        self.__bound_texture = self.__textures[idx]
+        self.__bound_texture.begin()
+
+    def end(self):
+        """ Unset the state. """
+        assert self.__bound_texture is not None
+        self.__bound_texture.end()
+        self.__bound_texture = None
+
     def get_width(self):
-        return self.__surface.get_width()
+        """ The texture width. """
+        return self.__textures[0].get_width()
+
     def get_height(self):
-        return self.__surface.get_height()
+        """ The texture height. """
+        return self.__textures[0].get_height()
 
 class PygameOpenGLRenderer(Renderer):
     """ A pygame software renderer. """
@@ -38,7 +92,8 @@ class PygameOpenGLRenderer(Renderer):
         GL.glMatrixMode(GL.GL_PROJECTION)
         GL.glLoadIdentity()
         GL.glOrtho(0, self.__surface.get_width(), self.__surface.get_height(), 0, 0, 1)
-        GL.glDisable(GL.GL_DEPTH_TEST)
+        GL.glEnable(GL.GL_BLEND)
+        GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
 
     def flip_buffers(self):
         """ Update the pygame display. """
@@ -141,25 +196,35 @@ class PygameOpenGLRenderer(Renderer):
         """ Render an animation. """
         width = job.length_to_screen(job.anim.frames.get_width())
         height = job.length_to_screen(job.anim.frames.get_height())
-        GL.glColor3f(1, 1, 1)
+        job.anim.frames.begin(job.anim.timer)
         GL.glBegin(GL.GL_QUADS)
+        GL.glTexCoord2f(0, 0)
         GL.glVertex2f(*(job.position + Vec2d(-width/2, -height/2).rotated(math.radians(-job.orientation))))
+        GL.glTexCoord2f(1, 0)
         GL.glVertex2f(*(job.position + Vec2d(width/2, -height/2).rotated(math.radians(-job.orientation))))
+        GL.glTexCoord2f(1, 1)
         GL.glVertex2f(*(job.position + Vec2d(width/2, height/2).rotated(math.radians(-job.orientation))))
+        GL.glTexCoord2f(0, 1)
         GL.glVertex2f(*(job.position + Vec2d(-width/2, height/2).rotated(math.radians(-job.orientation))))
         GL.glEnd()
+        job.anim.frames.end()
 
     def render_RenderJobImage(self, job):
         """ Render an image. """
         width = job.length_to_screen(job.image.get_width())
         height = job.length_to_screen(job.image.get_height())
-        GL.glColor3f(1, 1, 1)
+        job.image.begin()
         GL.glBegin(GL.GL_QUADS)
+        GL.glTexCoord2f(0, 0)
         GL.glVertex2f(*(job.position + Vec2d(0, 0)))
+        GL.glTexCoord2f(1, 0)
         GL.glVertex2f(*(job.position + Vec2d(width, 0)))
+        GL.glTexCoord2f(1, 1)
         GL.glVertex2f(*(job.position + Vec2d(width, height)))
+        GL.glTexCoord2f(0, 1)
         GL.glVertex2f(*(job.position + Vec2d(0, height)))
         GL.glEnd()
+        job.image.end()
 
     def render_RenderJobWarning(self, job):
         """ Render a warning on the screen. """
