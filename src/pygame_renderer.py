9342aff963fb9b84e1b3ad2abcc640ef00aacf4c
Nathan Woodward <nathanrichardwoodward@gmail.com>
2018-12-24 08:09:27 +0000

    remove hack for pynk

--- src/pygame_renderer.py
+++ src/pygame_renderer.py
@@ -2,13 +2,7 @@ import pygame
 
 from .renderer import *
 
-try:
-    import pynk.nkpygame
-except:
-    # Note: this lets us run the unit tests for now, as I am having 
-    # trouble getting pynk to install in the travis environment. This 
-    # should be removed when that is fixed.
-    print "Failed to import pynk. GUI rendering won't work."
+import pynk.nkpygame
 
 class PygameRenderer(Renderer):
     """ A pygame software renderer. """
